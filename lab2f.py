37e11e514fa8742bff114528856d11aa80affdcd
Prince Arkoh <aparkoh@myseneca.ca>
2026-05-15 16:49:42 -0400

    Added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Author: Prince Asamoah Arkoh
+# Author ID: aparkoh@myseneca.ca
+# Date Created: 2026/05/16
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
