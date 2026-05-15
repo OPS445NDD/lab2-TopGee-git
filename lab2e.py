6483db8ef0bb5432de612c96b3e9f47c8c89ff65
Prince Arkoh <aparkoh@myseneca.ca>
2026-05-15 16:49:19 -0400

    Added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Prince Asamoah Arkoh
+# Author ID: aparkoh@myseneca.ca
+# Date Created: 2026/05/16
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
